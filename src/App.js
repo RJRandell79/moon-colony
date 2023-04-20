8872e1763a0d0e0ae95f3a05a0dff344c8673669
Rob Randell <rob.randell@icloud.com>
2023-04-20 13:13:38 +0100

    refactor: switch to functional component for `App`

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.scss';
 
@@ -9,36 +9,34 @@ import Refinery from './departments/Refinery';
 
 //source ~/.nvm/nvm.sh
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date(Date.UTC(1970, 0, 1, 0, 0, 0, 0)),
-      elements: [
-          { element: 'Aluminum', amount: 0, rate: 0 },
-          { element: 'Calcium', amount: 0, rate: 0 },
-          { element: 'Hydrogen', amount: 0, rate: 3 },
-          { element: 'Iron', amount: 0, rate: 1 },
-          { element: 'Magnesium', amount: 0, rate: 0 },
-          { element: 'Oxygen', amount: 0, rate: 0 },
-          { element: 'Potassium', amount: 0, rate: 0 },
-          { element: 'Silicon', amount: 0, rate: 0 },
-          { element: 'Sodium', amount: 0, rate: 0 },
-          { element: 'Titanium', amount: 0, rate: 0 }
-      ],
-      refineryActive: '0'
-    }
+const App = () => {
+  
+  const [dateTime, setDateTime] = useState(
+    new Date(Date.UTC(1970, 0, 1, 0, 0, 0, 0))
+  );
+  const [elements, setElements] = useState(
+    [
+      { element: 'Aluminum', amount: 0, rate: 0 },
+      { element: 'Calcium', amount: 0, rate: 0 },
+      { element: 'Hydrogen', amount: 0, rate: 3 },
+      { element: 'Iron', amount: 0, rate: 1 },
+      { element: 'Magnesium', amount: 0, rate: 0 },
+      { element: 'Oxygen', amount: 0, rate: 0 },
+      { element: 'Potassium', amount: 0, rate: 0 },
+      { element: 'Silicon', amount: 0, rate: 0 },
+      { element: 'Sodium', amount: 0, rate: 0 },
+      { element: 'Titanium', amount: 0, rate: 0 }
+    ]
+  );
+  const [refineryActive, setRefineryActive] = useState('0');
+  
+  const toggleRefineryActive = (value) => {
+    setRefineryActive(value);
   }
 
-  refineryActive = (value) => {
-    this.setState(prevState => ({
-      refineryActive: value
-    }));
-    console.log(this.state.refineryActive);
-  }
-
-  mineElements = () => {
-    if(this.state.refineryActive !== '0') {
+  /*
+  const mineElements = () => {
+    if(refineryActive !== '0') {
       const updatedElements = this.state.elements.map((element) => {
           return { ...element, amount: element.amount + element.rate }
       });
@@ -46,75 +44,66 @@ class App extends Component {
       console.log('mining');
     }
   }
+  */
 
-  advanceDay = () => {
-    const newDate = new Date(this.state.date);
+  const advanceDay = () => {
+    const newDate = new Date(dateTime);
     newDate.setDate(newDate.getDate() + 1);
-    this.setState(prevState => ({
-      date: newDate
-    }));
-    this.mineElements();
+    setDateTime(newDate);
+    //this.mineElements();
     console.log('day+');
   }
 
-  advanceHour = () => {
-    const newTime = new Date(this.state.date);
+  const advanceHour = () => {
+    const newTime = new Date(dateTime);
     newTime.setTime(newTime.getTime() + 3600000);
-    this.setState(prevState => ({
-      date: newTime
-    }));
+    setDateTime(newTime);
     console.log('hour+');
   }
 
-  advanceSecond = () => {
-    const newSecond = new Date(this.state.date);
-    newSecond.setTime(newSecond.getTime() + 1000);
-    this.setState(prevState => ({
-      date: newSecond
-    }));
-    //console.log('second+');
-  }
+  useEffect(() => {
+    const interval = setInterval(
+      () => setDateTime((dateTime) => {
+        const newSecond = new Date(dateTime);
+        newSecond.setSeconds(newSecond.getSeconds() + 1);
+        return newSecond;
+      }), 1000);
+      return () => clearInterval(interval);
+  }, []);  
 
-  componentDidMount() {
-    setInterval(() => {
-      this.advanceSecond()
-    }, 1000);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Router>
-            <div className="header-toolbar">
-              <ul>
-                <li>
-                  <Link to="/power">Power</Link>
-                </li>
-                <li>
-                  <Link to="/refinery">Refinery</Link>
-                </li>
-                <li>
-                  <button type="button" onClick={this.advanceDay}>Advance day</button>
-                </li>
-                <li>
-                  <button type="button" onClick={this.advanceHour}>Advance hour</button>
-                </li>
-                <li>
-                  <p>{this.state.date.toUTCString().slice(5, -4)} </p>
-                </li>
-              </ul>
-            </div>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="power" element={<Power />} />
-              <Route path="refinery" element={<Refinery toggleRefinery={this.refineryActive.bind(this)} refineryActive={this.state.refineryActive} elements={this.state.elements} />} />
-            </Routes>
-          </Router>
-        </header>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Router>
+          <div className="header-toolbar">
+            <ul>
+              <li>
+                <Link to="/power">Power</Link>
+              </li>
+              <li>
+                <Link to="/refinery">Refinery</Link>
+              </li>
+              <li>
+                <button type="button" onClick={() => advanceDay()}>Advance day</button>
+              </li>
+              <li>
+                <button type="button" onClick={() => advanceHour()}>Advance hour</button>
+              </li>
+              <li>
+                <p>{dateTime.toUTCString().slice(5, -4)} </p>
+              </li>
+            </ul>
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="power" element={<Power />} />
+            <Route path="refinery" element={<Refinery elements={elements} />} />
+          </Routes>
+        </Router>
+      </header>
+    </div>
+  );
+  
 }
 
 export default App;
